2744cc264d3791f380e5babbf07d7be568a5077d
Ian <ianlau_29@yahoo.com>
2022-05-11 13:39:53 -0700

    Committing InClass01

---
+++ app/src/main/java/com/example/practice/in_class_01.java
@@ -0,0 +1,88 @@
+package com.example.practice;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class in_class_01 extends AppCompatActivity {
+
+    Button calculateBMI;
+    EditText weight, feet, inches;
+    TextView display1, display2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_class_01);
+        setTitle("BMI Calculator");
+
+        System.out.println("#####");
+        Log.i("demo", "#####");
+
+        calculateBMI = findViewById(R.id.buttonCalculate);
+        weight = findViewById(R.id.editTextWeight);
+        feet = findViewById(R.id.editTextFeet);
+        inches = findViewById(R.id.editTextInches);
+        display1 = findViewById(R.id.displayText);
+        display2 = findViewById(R.id.displayText2);
+
+        calculateBMI.setText("@strings/BMIButton");
+
+        // calculate BMI
+        calculateBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+
+                    int w = Integer.parseInt(weight.getText().toString());
+                    int f = Integer.parseInt(feet.getText().toString());
+                    int i = Integer.parseInt(inches.getText().toString());
+
+                    if (w < 0 || f < 0 || i < 0) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    int totalHeight = (f * 12) + i;
+                    double BMI = ((w / (Math.pow(totalHeight, 2))) * 703);
+                    //float BMI = (w + totalHeight);
+                    Log.i("demo",w + " " + f + " " + i + " " + BMI + " " + totalHeight);
+                    String display1Msg = "Your BMI: " + String.format("%.1f", BMI);
+
+                    display1.setText(display1Msg);
+
+                    if (BMI < 18.5) {
+
+                        display2.setText("You are underweight");
+
+                    } else if(BMI >= 18.5 && BMI <= 24.9) {
+
+                        display2.setText("You are a normal weight");
+
+                    } else if(BMI >= 25 && BMI <= 29.9) {
+
+                        display2.setText("You are overweight");
+
+                    } else {
+
+                        display2.setText("You are obese");
+
+                    }
+
+                    Toast.makeText(in_class_01.this, "BMI Calculated",
+                            Toast.LENGTH_LONG).show();
+
+                } catch (IllegalArgumentException e) {
+                    Toast.makeText(in_class_01.this, "Invalid Inputs",
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/practice/in_class_practice.java
+++ app/src/main/java/com/example/practice/in_class_practice.java
@@ -2,6 +2,7 @@ package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,7 @@ public class in_class_practice extends AppCompatActivity {
 
     Button button;
     Button button2;
+    Button inClass01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +26,12 @@ public class in_class_practice extends AppCompatActivity {
         // instantiate IDs
         button = findViewById(R.id.button);
         button2 = findViewById(R.id.button2);
+        inClass01 = findViewById(R.id.button5);
 
         // set texts
         button.setText("Log Cat");
         button2.setText("Toast");
+        inClass01.setText("In Class 01");
 
         // log cat button listener
         button.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +47,16 @@ public class in_class_practice extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(in_class_practice.this, "Now push to GitHub and Submit!",
                         Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // button to go to in class assignment 1
+        inClass01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toInClass01 = new Intent(in_class_practice.this,
+                        in_class_01.class);
+                startActivity(toInClass01);
             }
         });
     }

ac15ded630237708857d26ec786bce3465264bd7
Ian <ianlau_29@yahoo.com>
2022-06-15 17:02:59 -0700

    In Class 09 - Permission issue with register fragment camera

--- app/src/main/java/com/example/practice/ChatRegisterFragment.java
+++ app/src/main/java/com/example/practice/ChatRegisterFragment.java
@@ -1,12 +1,26 @@
 package com.example.practice;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +36,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,13 +50,17 @@ import java.util.Map;
 public class ChatRegisterFragment extends Fragment implements View.OnClickListener {
 
     private EditText name, email, password, repeatPassword, firstName, lastName;
-    private Button register;
+    private Button register, takePhoto;
     private String userName, userEmail, userPassword, userRepeatedPassword, userFirstName, userLastName;
     private IregisterFragmentAction mListener;
     private FirebaseFirestore db;
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    Uri photoURI;
 
     public ChatRegisterFragment() {
         // Required empty public constructor
@@ -86,6 +105,36 @@ public class ChatRegisterFragment extends Fragment implements View.OnClickListen
         lastName = view.findViewById(R.id.editTextLastName);
         register = view.findViewById(R.id.chatRegisterButton);
         register.setOnClickListener(this);
+        takePhoto = view.findViewById(R.id.chatRegisterTakePhotoButton);
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Bundle b = result.getData().getExtras();
+                    Bitmap bitmap = (Bitmap) b.get("data");
+                    photoURI = getImageUri(getActivity(), bitmap);
+                }
+            }
+        });
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    try {
+                        activityResultLauncher.launch(intent);
+                    } catch (ActivityNotFoundException e) {
+                        Toast.makeText(getActivity(), "No camera available on this device.",
+                                Toast.LENGTH_LONG).show();
+                    }
+
+
+                }
+        });
 
         return view;
     }
@@ -128,11 +177,15 @@ public class ChatRegisterFragment extends Fragment implements View.OnClickListen
                                     newUser.put("username", userName);
                                     newUser.put("firstName", userFirstName);
                                     newUser.put("lastName", userLastName);
+                                    if (photoURI != null) {
+                                        newUser.put("profilePicture", photoURI);
+                                    }
                                     db.collection("users").document(userName).set(newUser);
 
 //                                    Adding name to the FirebaseUser...
                                     UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
                                             .setDisplayName(userName)
+                                            .setPhotoUri(photoURI)
                                             .build();
 
                                     mUser.updateProfile(profileChangeRequest)
@@ -153,5 +206,12 @@ public class ChatRegisterFragment extends Fragment implements View.OnClickListen
 
     public interface IregisterFragmentAction {
         void registerDone(FirebaseUser mUser);
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes); // Used for compression rate of the Image : 100 means no compression
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "xyz", null);
+        return Uri.parse(path);
     }
 }
